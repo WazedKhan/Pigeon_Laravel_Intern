049e31617633b18651e63a10f9c38670215d2630
WazedKhan <51821594+WazedKhan@users.noreply.github.com>
2021-10-24 10:42:53 +0600

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_170,w_170,f_auto,b_white,q_auto:eco,dpr_1/ikqra03zdnggljdu5vv0" class = "rounded-circle">
+        </div>
+        <div class="col-9 pt-5">
+            <div><h1>freecodecamp</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>415 </strong>posts</div>
+                <div class="pr-5"><strong>83.4k </strong>followers</div>
+                <div class="pr-5"><strong>315 </strong>following</div>
+            </div>
+            <div class="pt-4 font-weight-bold">freeCodeCamp.org</div>
+            <div>We're a global community of millions of people learning to code together. We're an open source, donor-supported, 501(c)(3) nonprofit.</div>
+        </div>
+    </div>
+</div>
+@endsection
