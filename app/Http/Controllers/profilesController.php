578f27f89529fc4020be071d36fe224a7e775707
WazedKhan <51821594+WazedKhan@users.noreply.github.com>
2021-10-25 20:49:03 +0600

    Added profile model and migrated

--- app/Http/Controllers/profilesController.php
+++ app/Http/Controllers/profilesController.php
@@ -2,14 +2,16 @@
 
 namespace App\Http\Controllers;
 
-
+use App\User;
 use Illuminate\Http\Request;
 
 class profilesController extends Controller
 {
     public function index($user)
     {
-        dd($user);
-        return view('home');
+        $user = User::find($user);
+        return view('home', [
+            'user' => $user, 
+        ]);
     }
 }
